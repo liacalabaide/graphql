9f698fabaef70ce99b780adb89f10e7d00af457a
liacalabaide <liana.calabaide@mercadolivre.com>
2023-03-31 09:08:44 -0300

    graphql example with golang

---
+++ internal/database/course.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/google/uuid"
+	"log"
+)
+
+type Course struct {
+	db          *sql.DB
+	ID          string
+	Name        string
+	Description string
+	CategoryID  string
+}
+
+func NewCourse(db *sql.DB) *Course {
+	return &Course{db: db}
+}
+
+func (c *Course) Create(name, description, categoryID string) (Course, error) {
+	id := uuid.New().String()
+	_, err := c.db.Exec("INSERT INTO course (id, name, description, category_id) values ($s1, $s2, $s3, $s4)", id, name, description, categoryID)
+	log.Printf("Erro -", err)
+	if err != nil {
+		return Course{}, err
+	}
+	return Course{ID: id, Name: name, Description: description, CategoryID: categoryID}, nil
+}
+
+func (c *Course) FindAll() ([]Course, error) {
+	rows, err := c.db.Query("SELECT id, name, description, category_id FROM course")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	courses := []Course{}
+	for rows.Next() {
+		var id, name, description, categoryID string
+		if err := rows.Scan(&id, &name, &description, &categoryID); err != nil {
+			return nil, err
+		}
+		courses = append(courses, Course{ID: id, Name: name, Description: description, CategoryID: categoryID})
+	}
+	return courses, nil
+}
